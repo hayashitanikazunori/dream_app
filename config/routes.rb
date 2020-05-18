e7234ae3fdaaaa3721a5ac0ddad70fd2b0d765d5
hayashitani-kazunori <fworkkaz@gmail.com>
2020-05-18 21:22:47 +0900

    users show create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  resources :users, only: [:show]
   root to: 'dreams#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
