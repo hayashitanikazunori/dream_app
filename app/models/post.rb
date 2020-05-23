ecba7257e84d248540ca181e8eb5e04c4e4b6ff6
hayashitani-kazunori <fworkkaz@gmail.com>
2020-05-23 16:47:11 +0900

    development.rb amazon > local change

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
   validates :image, presence: true
-  validates :comment, presence: true
+  validates :comment, presence: true, length: { maximum: 100 }
   has_one_attached :image
 end
